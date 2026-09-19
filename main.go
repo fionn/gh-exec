e85313909e02535112d2bbba17144452ad130054
Fionn Fitzmaurice <git@fionn.computer>
2026-09-19 22:56:33 +0800

    Elaborate on getToken's approach

--- main.go
+++ main.go
@@ -8,10 +8,26 @@ import (
 	"strings"
 )
 
-// getToken is adapted from pkg/auth/auth.go's tokenFromGh. This seems weird,
-// the point of running as a gh extension is we inherit the authentication
-// context -- but this is literally how the gh extension itself derives the
-// token.
+// getToken is adapted from go-gh/pkg/auth/auth.go's tokenFromGh. This seems
+// weird, the point of running as a gh extension is we inherit the
+// authentication context -- but this is how go-gh derives the token (when using
+// secure storage).
+//
+// The full flow for cli/gh is:
+// * try to retrieve the token from the environment,
+// * try to retrieve the token from disk,
+// * try to retrieve the token from secure storage.
+// These are all implemented on top of gh's configuration struct, so importing
+// this functionality isn't ideal. So instead look at
+// cli/go-gh/pkg/auth/auth.go, which we could use like
+//
+//	token, _ := auth.TokenForHost("github.com")
+//
+// to get the token, with an additional direct dependency. This would try to
+// retrieve the token in the same order as gh, but on the last option, retrieval
+// from secure storage (which we expect to be using in any case), it shells out
+// to gh like we do (and wastes time checking if gh is executable, something we
+// can safely skip).
 func getToken() (string, error) {
 	cmd := exec.Command("gh", "auth", "token")
 	result, err := cmd.Output()
